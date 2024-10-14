e7b502bcb26d853e1cc0a3c8ac06bb11569b8fa2
Eugene Zaidun <vimo@eugenes-imac.kpn>
2024-10-14 21:40:12 +0200

    wip

--- packages/chat-frontend/vite.config.ts
+++ packages/chat-frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/frontend',
+  cacheDir: '../../node_modules/.vite/packages/chat-frontend',
 
   server: {
     port: 4200,
@@ -26,7 +26,7 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../../dist/packages/frontend',
+    outDir: '../../dist/packages/chat-frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
